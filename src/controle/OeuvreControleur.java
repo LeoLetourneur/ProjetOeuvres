7f4f7b003663b3b919ca38f29b36298a580b7d68
Letourneur Léo <leo.letourneur@etu.univ-lyon1.fr>
2016-02-19 14:05:01 +0100

    Correction bug oeuvre

--- src/controle/OeuvreControleur.java
+++ src/controle/OeuvreControleur.java
@@ -18,7 +18,7 @@ import meserreurs.*;
 /**
  * Servlet implementation class Controleur
  */
-@WebServlet("/Oeuvre/")
+@WebServlet("/Oeuvre")
 public class OeuvreControleur extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String ACTION_TYPE = "action";
